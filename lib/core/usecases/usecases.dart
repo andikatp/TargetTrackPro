e4bb6df074f0a64fb39d1279a2c32c07bd0ac177
andikatp <triprasetya_andika@yahoo.com>
2024-03-08 12:26:03 +0700

    feat(product): Add product repository

--- lib/core/usecases/usecases.dart
+++ lib/core/usecases/usecases.dart
@@ -1,4 +1,13 @@
+import 'package:business/core/utils/typedef.dart';
+
 abstract class UseCaseWithParams<Type, Params>{
   const UseCaseWithParams();
-  
+
+  ResultFuture<Type> call(Params params);
+}
+
+abstract class UseCaseWithOutParams<Type>{
+  const UseCaseWithOutParams();
+
+  ResultFuture<Type> call();
 }
