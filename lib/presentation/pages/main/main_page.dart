548d93d3b624b3dc32ba86de7f3bb8f9f3ff01b6
andikatp <triprasetya_andika@yahoo.com>
2024-03-09 14:52:27 +0700

    feat(product): put product inside tabbar

---
+++ lib/presentation/pages/main/main_page.dart
@@ -0,0 +1,50 @@
+import 'package:business/core/res/colours.dart';
+import 'package:business/presentation/pages/product/product_page.dart';
+import 'package:flutter/material.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('TargetTrackPro'),
+          centerTitle: false,
+          actions: [
+            IconButton(
+              icon: const Icon(
+                Icons.person,
+                color: Colours.whiteColor,
+              ),
+              onPressed: () {},
+            ),
+          ],
+          bottom: TabBar(
+            tabs: const [
+              Tab(text: 'Product'),
+              Tab(text: 'Business'),
+              Tab(text: 'Marketing'),
+            ],
+            unselectedLabelColor: Colours.whiteColor.withOpacity(0.6),
+            labelColor: Colours.whiteColor,
+          ),
+        ),
+        body: const TabBarView(
+          children: [
+            ProductPage(),
+            Text('data2'),
+            Text('data3'),
+          ],
+        ),
+      ),
+    );
+  }
+}
