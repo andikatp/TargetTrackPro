34ade20857c226919ffd444a61e063bbb6b67269
Andika Tri Prasetya <78133832+andikatp@users.noreply.github.com>
2024-03-09 16:21:43 +0700

    Feat/marketing (#25)
    * feat(marketing): domain layer for marketing
    
    * feat(marketing): data layer including dao and repo

--- lib/data/datasource/app_database.dart
+++ lib/data/datasource/app_database.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'package:business/core/utils/enums.dart';
 import 'package:business/data/datasource/DAO/business/business_target_dao.dart';
+import 'package:business/data/datasource/DAO/marketing/marketing_target_dao.dart';
 import 'package:business/data/datasource/DAO/product/product_target_dao.dart';
 import 'package:business/data/models/core/target_model.dart';
 import 'package:floor/floor.dart';
@@ -14,6 +15,7 @@ part 'app_database.g.dart';
 abstract class AppDatabase extends FloorDatabase {
   ProductTargetDao get productTargetDao;
   BusinessTargetDao get businessTargetDao;
+  MarketingTargetDao get marketingTargetDao;
 }
 
 class DateTimeConverter extends TypeConverter<DateTime, int> {
--- lib/data/datasource/app_database.g.dart
+++ lib/data/datasource/app_database.g.dart
@@ -66,6 +66,8 @@ class _$AppDatabase extends AppDatabase {
 
   BusinessTargetDao? _businessTargetDaoInstance;
 
+  MarketingTargetDao? _marketingTargetDaoInstance;
+
   Future<sqflite.Database> open(
     String path,
     List<Migration> migrations, [
@@ -106,6 +108,12 @@ class _$AppDatabase extends AppDatabase {
   BusinessTargetDao get businessTargetDao {
     return _businessTargetDaoInstance ??=
         _$BusinessTargetDao(database, changeListener);
+  }
+
+  @override
+  MarketingTargetDao get marketingTargetDao {
+    return _marketingTargetDaoInstance ??=
+        _$MarketingTargetDao(database, changeListener);
   }
 }
 
@@ -283,6 +291,95 @@ class _$BusinessTargetDao extends BusinessTargetDao {
 
   @override
   Future<void> deleteBusinessTarget(TargetModel target) async {
+    await _targetModelDeletionAdapter.delete(target);
+  }
+}
+
+class _$MarketingTargetDao extends MarketingTargetDao {
+  _$MarketingTargetDao(
+    this.database,
+    this.changeListener,
+  )   : _queryAdapter = QueryAdapter(database),
+        _targetModelInsertionAdapter = InsertionAdapter(
+            database,
+            'target',
+            (TargetModel item) => <String, Object?>{
+                  'id': item.id,
+                  'name': item.name,
+                  'category': item.category.index,
+                  'weight': item.weight,
+                  'status': item.status.index,
+                  'type': item.type.index,
+                  'startDate': _dateTimeConverter.encode(item.startDate),
+                  'endDate': _dateTimeConverter.encode(item.endDate)
+                }),
+        _targetModelUpdateAdapter = UpdateAdapter(
+            database,
+            'target',
+            ['id'],
+            (TargetModel item) => <String, Object?>{
+                  'id': item.id,
+                  'name': item.name,
+                  'category': item.category.index,
+                  'weight': item.weight,
+                  'status': item.status.index,
+                  'type': item.type.index,
+                  'startDate': _dateTimeConverter.encode(item.startDate),
+                  'endDate': _dateTimeConverter.encode(item.endDate)
+                }),
+        _targetModelDeletionAdapter = DeletionAdapter(
+            database,
+            'target',
+            ['id'],
+            (TargetModel item) => <String, Object?>{
+                  'id': item.id,
+                  'name': item.name,
+                  'category': item.category.index,
+                  'weight': item.weight,
+                  'status': item.status.index,
+                  'type': item.type.index,
+                  'startDate': _dateTimeConverter.encode(item.startDate),
+                  'endDate': _dateTimeConverter.encode(item.endDate)
+                });
+
+  final sqflite.DatabaseExecutor database;
+
+  final StreamController<String> changeListener;
+
+  final QueryAdapter _queryAdapter;
+
+  final InsertionAdapter<TargetModel> _targetModelInsertionAdapter;
+
+  final UpdateAdapter<TargetModel> _targetModelUpdateAdapter;
+
+  final DeletionAdapter<TargetModel> _targetModelDeletionAdapter;
+
+  @override
+  Future<List<TargetModel>> getMarketingTargets() async {
+    return _queryAdapter.queryList('SELECT * FROM target WHERE type = 0',
+        mapper: (Map<String, Object?> row) => TargetModel(
+            id: row['id'] as String,
+            name: row['name'] as String,
+            category: Category.values[row['category'] as int],
+            weight: row['weight'] as int,
+            status: Status.values[row['status'] as int],
+            type: TargetType.values[row['type'] as int],
+            startDate: _dateTimeConverter.decode(row['startDate'] as int),
+            endDate: _dateTimeConverter.decode(row['endDate'] as int)));
+  }
+
+  @override
+  Future<void> saveMarketingTarget(TargetModel target) async {
+    await _targetModelInsertionAdapter.insert(target, OnConflictStrategy.abort);
+  }
+
+  @override
+  Future<void> editMarketingTarget(TargetModel target) async {
+    await _targetModelUpdateAdapter.update(target, OnConflictStrategy.replace);
+  }
+
+  @override
+  Future<void> deleteMarketingTarget(TargetModel target) async {
     await _targetModelDeletionAdapter.delete(target);
   }
 }
