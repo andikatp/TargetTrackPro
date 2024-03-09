2b58952d5318e8e7a5d1ce044d93cec9549c3dcb
andikatp <triprasetya_andika@yahoo.com>
2024-03-09 18:15:00 +0700

    feat(splash): finishing data layer

---
+++ lib/data/datasource/splash/splash_local_datasource.dart
@@ -0,0 +1,24 @@
+import 'package:business/core/errors/exceptions.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+abstract class SplashLocalDataSource {
+  const SplashLocalDataSource();
+
+  Future<bool> checkUserIsLoggedIn();
+}
+
+class SplashLocalDataSourceImpl implements SplashLocalDataSource {
+  SplashLocalDataSourceImpl({required SharedPreferences preference})
+      : _preference = preference;
+  final SharedPreferences _preference;
+
+  @override
+  Future<bool> checkUserIsLoggedIn() async {
+    try {
+      final result = _preference.getString('isLoggedIn');
+      return result != null;
+    } catch (e) {
+      throw CacheException(message: e.toString());
+    }
+  }
+}
