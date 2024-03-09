08f9217e51dc4c83eb8107fc9f35d3aa8dfc5c7f
andikatp <triprasetya_andika@yahoo.com>
2024-03-09 15:15:17 +0700

    feat(business): Add dependency injector and apply it in main

--- lib/core/services/dependency_container.dart
+++ lib/core/services/dependency_container.dart
@@ -1,10 +1,17 @@
 import 'package:business/data/datasource/app_database.dart';
+import 'package:business/data/repositories/business/business_repositories_impl.dart';
 import 'package:business/data/repositories/product/product_repositories_impl.dart';
+import 'package:business/domain/repositories/business/business_repositories.dart';
 import 'package:business/domain/repositories/product/product_repositories.dart';
+import 'package:business/domain/usecases/business/delete_business_target.dart';
+import 'package:business/domain/usecases/business/edit_business_target.dart';
+import 'package:business/domain/usecases/business/get_business_targets.dart';
+import 'package:business/domain/usecases/business/save_business_target.dart';
 import 'package:business/domain/usecases/product/delete_product_target.dart';
 import 'package:business/domain/usecases/product/edit_product_target.dart';
 import 'package:business/domain/usecases/product/get_product_targets.dart';
 import 'package:business/domain/usecases/product/save_product_target.dart';
+import 'package:business/presentation/blocs/business/bloc/business_bloc.dart';
 import 'package:business/presentation/blocs/product/bloc/product_bloc.dart';
 import 'package:get_it/get_it.dart';
 
