44ee5250798c40a0e59927ba40810dd604cddff3
andikatp <triprasetya_andika@yahoo.com>
2024-03-12 09:25:39 +0700

    feat(main): register at di and main provider

--- lib/presentation/blocs/main/cubit/main_cubit.dart
+++ lib/presentation/blocs/main/cubit/main_cubit.dart
@@ -1,12 +1,21 @@
+import 'package:business/domain/usecases/main/log_out.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'main_state.dart';
 
 class MainCubit extends Cubit<MainState> {
-  MainCubit() : super(const MainInitial());
+  MainCubit({
+    required LogOut logOut,
+  })  : _logOut = logOut,
+        super(const MainInitial());
 
-  Future<void> logOut()async{
+  final LogOut _logOut;
+
+  Future<void> logOut() async {
     emit(const MainLoading());
+    final result = await _logOut();
+    result.fold((failure) => emit(MainError(message: failure.errorMessage)),
+        (_) => emit(const MainLoggedOut()),);
   }
 }
